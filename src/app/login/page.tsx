fa775b3cb49012e3b055501d02ce6c6789850f28
Kabir Lakhadive <prabal9975@gmail.com>
2025-08-22 17:56:58 +0530

    Integrated login and signup with supabase.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,41 +8,76 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label";
 
 import Link from "next/link";
 
 export default function LoginPage(){
+
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    const supabase = createClient();
+
+    const handleLogin = async () =>{
+        setError(null);
+        setLoading(true);
+
+        const {error} = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if(error){
+            setError(error.message);
+            setLoading(false);
+            return;
+        }
+        else{
+            router.push('/dashboard');
+            router.refresh();
+        }
+    };
+
+
+
+
     return (
-        <div>
-            <Card>
+        <div className="flex justify-center items-center h-full">
+            <Card className="w-full max-w-sm">
                 <CardHeader>
-                    <CardTitle>Login</CardTitle>
-                    <CardDescription>
-            Enter your email below to login to your account.
-          </CardDescription>
+                    <CardTitle className="text-3xl">Login</CardTitle>
+                    <CardDescription >
+                        Enter your email below to login to your account.
+                    </CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <div>
-                        <Label>
+                <CardContent className="grid gap-6">
+                    {error && <p className="text-red-500 text-center">{error}</p>}
+                    <div className="grid gap-4">
+                        <Label htmlFor="email">
                             Email
                         </Label>
-                        <Input id="email" type="email" placeholder="johndoe@exmaple.com"/>
+                        <Input id="email" type="email" placeholder="johndoe@exmaple.com" required value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
-                    <div>
-                        <Label>
+                    <div className="grid gap-4">
+                        <Label htmlFor="password">
                           Password
                         </Label>
-                        <Input id="password" type="password" required />
+                        <Input id="password" type="password" required value = {password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
-                    <Button className="w-full">
-                        Login
+                    <Button className="w-full" onClick={handleLogin} >
+                        {loading ? 'Loading...' : 'Sign in'}
                     </Button>
                 </CardContent>
 
-                <div>
-                    Don't have an account?  
+                <div className="text-center">
+                    Don't have an account? {' '}  
                     <Link href="/signup" className="underline">
                     Sign up
                     </Link>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,30 +10,69 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createClient } from "@/lib/supabase/client";
 import Link from 'next/link';
 
 export default function SignupPage() {
+
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null);
+    const [successMessage , setSuccessMessage] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const supabase = createClient();
+    
+
+    const handleSignUp = async () =>{
+        setError(null);
+        setLoading(true);
+        setSuccessMessage(null);
+
+        const{data,error} = await supabase.auth.signUp({
+            email,
+            password,
+        });
+
+        if(error){
+            setError(error.message);
+        }else{
+            setSuccessMessage("Successfully Created and account.")
+            setTimeout (() => router.push('/login'), 3000)
+            setLoading(false);
+        }
+
+    }
+
+
+
+
+
   return (
-    <div >
-      <Card >
+  <div className="flex justify-center items-center h-full">
+      <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle>Sign Up</CardTitle>
+          <CardTitle className="text-3xl">Sign Up</CardTitle>
           <CardDescription>
             Enter your information to create an account.
           </CardDescription>
         </CardHeader>
-        <CardContent >
-          <div >
-            <Label>Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
+        <CardContent className="grid gap-6">
+          <div className="grid gap-4">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="johndoe@example.com" required value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
-          <div >
-            <Label>Password</Label>
-            <Input id="password" type="password" required />
+          <div className="grid gap-4">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
           </div>
-          <Button className="w-full">Create account</Button>
+          <Button className="w-full" onClick={handleSignUp}>
+            {loading? `loading` : `Create Account`}
+          </Button>
         </CardContent>
-        <div className="mb-6 text-center text-sm">
+        <div className="text-center ">
           Already have an account?{' '}
           <Link href="/login" className="underline">
             Login
